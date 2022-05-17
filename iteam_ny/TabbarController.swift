24a4c0a486af0041e6e1aa087e089c3abce1e4bd
khnpoq@naver.com <khnpoq@naver.com>
2022-05-18 04:39:57 +0900

    [DESIGN] 탭바 커스텀

--- iteam_ny/TabbarController.swift
+++ iteam_ny/TabbarController.swift
@@ -10,5 +10,31 @@ import UIKit
 class TabarController : UITabBarController{
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+    }
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        changeHeightOfTabbar()
+        changeShadow()
+        
+        
+    }
+    func changeShadow() {
+        //tabBar.shadowImage = UIImage()
+        
+        tabBar.shadowImage = UIImage()
+        tabBar.backgroundImage = UIImage()
+        tabBar.backgroundColor = .white
+        tabBar.layer.shadowOffset = CGSize(width: 0, height: 0)
+        tabBar.layer.shadowColor = UIColor.black.cgColor
+        tabBar.layer.shadowOpacity = 0.1
+    }
+    func changeHeightOfTabbar() {
+        var tabFrame = tabBar.frame
+        tabFrame.size.height = 90
+        tabFrame.origin.y = view.frame.size.height - 90
+        tabBar.frame = tabFrame
     }
 }
