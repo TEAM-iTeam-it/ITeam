e1afafb65fb5deaf4abc8f707417bbba9eca61a0
khnpoq@naver.com <khnpoq@naver.com>
2022-05-30 01:58:38 +0900

    [FIX] 홈탭 datepicker 디자인 및 기능 수정

--- iteam_ny/Home/SendTimePickerView.swift
+++ iteam_ny/Home/SendTimePickerView.swift
@@ -9,18 +9,27 @@ import UIKit
 
 class SendTimePickerView:UIViewController{
     @IBOutlet weak var saveBtn: UIButton!
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
     var select_Time: String = ""
     var dataN : Int = 0
     var numberI : String = ""
     var delegate : SendTimeDataDelegate?
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setUI()
+    }
+    
+    func setUI() {
+        self.view.layer.cornerRadius = 30
+        self.view.layer.masksToBounds = true
+    }
+    
     @IBAction func changeDatePicker(_ sender: UIDatePicker) {
         let datePickerView = sender
         let formatter = DateFormatter()
         formatter.dateFormat = "MM월 dd일 a hh시 mm분"
+        sender.minimumDate = Date()
         
         if dataN == 0{
             numberI = "첫번째"
@@ -35,16 +44,17 @@ class SendTimePickerView:UIViewController{
 //        self.delegate?.SendTimeDataDelegate(data: select_Time)
 //        self.dismiss(animated: true, completion: nil)
     }
+    
     @IBAction func saveAction(_ sender: UIButton) {
         if select_Time.isEmpty{
-                    saveBtn.isEnabled = false
-                    self.dismiss(animated: true, completion: nil)
-                }
-                else {
-                    self.delegate?.SendTimeData(data: numberI + select_Time )
-                    self.dismiss(animated: true, completion: nil)
-
-                }
+            saveBtn.isEnabled = false
+            self.dismiss(animated: true, completion: nil)
+        }
+        else {
+            self.delegate?.SendTimeData(data: numberI + select_Time )
+            self.dismiss(animated: true, completion: nil)
+            
+        }
         
     }
     
