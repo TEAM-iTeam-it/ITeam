1fb54be4ba297e6e4d5f5bb52c341e1ecdf6d601
angryeon7 <87401351+angryeon7@users.noreply.github.com>
2022-05-29 19:13:40 +0900

    [FEAT]마이페이지 재설정 기능 추가

--- iteam_ny/MyPageVC/MyPageModifyViewController.swift
+++ iteam_ny/MyPageVC/MyPageModifyViewController.swift
@@ -9,19 +9,58 @@ import UIKit
 import Firebase
 import FirebaseAuth
 import FirebaseDatabase
+import Kingfisher
 
 class MyPageModifyViewController: UIViewController  {
 
     @IBOutlet weak var positonLabel: UILabel!
     @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var myimg: UIImageView!
+    @IBOutlet weak var myNicknameTextField: UITextField!
     var dataBase: DatabaseReference! //Firebase Realtime Database
+    var ref = Database.database().reference().child("user")
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationController?.navigationBar.topItem?.title = ""
+        self.navigationController?.navigationBar.tintColor = .black
+        
+        let fancyImage = UIImage(systemName:"arrow.left")
+
+        var fancyAppearance = UINavigationBarAppearance()
+        fancyAppearance.configureWithDefaultBackground()
+        fancyAppearance.setBackIndicatorImage(fancyImage, transitionMaskImage: fancyImage)
+
+        navigationController?.navigationBar.scrollEdgeAppearance = fancyAppearance
+        
+        let userID = Auth.auth().currentUser?.uid
+        ref.child(userID!).child("userProfile").observeSingleEvent(of: .value, with: { snapshot in
+          // Get user value
+          let value = snapshot.value as? NSDictionary
+          let myname = value?["nickname"] as? String ?? ""
+            
+            self.myNicknameTextField.text = "\(myname)"
+
+        }) { error in
+          print(error.localizedDescription)
+        }
+        
+        let img = Storage.storage().reference().child("user_profile_image/\(Auth.auth().currentUser!.uid).jpg")
+        // Fetch the download URL
+        img.downloadURL { [self] url, error in
+            if let error = error {
+            } else {
+                myimg.kf.setImage(with: url)
+                myimg.layer.cornerRadius = myimg.frame.height/2
+            }
+        }
+        
+    }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
-        dataBase = Database.database().reference().child("user")
         let userID = Auth.auth().currentUser?.uid
-        dataBase.child(userID!).child("userProfile").observeSingleEvent(of: .value, with: { snapshot in
+        ref.child(userID!).child("userProfile").observeSingleEvent(of: .value, with: { snapshot in
           // Get user value
           let value = snapshot.value as? NSDictionary
           let partDetail = value?["partDetail"] as? String ?? ""
@@ -32,7 +71,7 @@ class MyPageModifyViewController: UIViewController  {
           print(error.localizedDescription)
         }
         
-        dataBase.child(userID!).observeSingleEvent(of: .value, with: { snapshot in
+        ref.child(userID!).observeSingleEvent(of: .value, with: { snapshot in
           // Get user value
           let value = snapshot.value as? NSDictionary
           let email = value?["email"] as? String ?? ""
@@ -57,4 +96,35 @@ class MyPageModifyViewController: UIViewController  {
         exit(0)
     }
     
+    @IBAction func saveDidTapped(_ sender: Any) {
+        
+        //myNicknameTextField.text
+        let userID = Auth.auth().currentUser!.uid
+        let myNickname = myNicknameTextField.text
+        ref.child(userID).child("userProfile").updateChildValues(["nickname":myNickname])
+        
+        
+    }
+    
+    
+    @IBAction func partDidTapped(_ sender: Any) {
+        
+        guard let MyPartVC = self.storyboard?.instantiateViewController(withIdentifier: "MyPartViewController") as? MyPartViewController else{return}
+        MyPartVC.delegate = self // AfterVC에서 생성한 delegate라는 변수를 통해서 델리게이트 지정.
+        MyPartVC.modalPresentationStyle = .fullScreen
+        present(MyPartVC, animated: true, completion: nil)
+        
+    
+    }
+}
+
+extension MyPageModifyViewController: PartDataDelegate {
+    func send(_ vc: UIViewController, Input value: String?) {
+       // valueLabel.text = value
+        let part = ""
+        let partDetail = ""
+        var partindex = value!.components(separatedBy: ",")
+        partindex[0] = part
+        partindex[1] = partDetail
+    }
 }
