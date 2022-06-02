863cede8ca7934b14e11fa1a4497e3ea758a0783
khnpoq@naver.com <khnpoq@naver.com>
2022-06-02 14:52:41 +0900

    [FIX] 팀확정 버튼 테스트 코드 구현

--- iteam_ny/Home/popVC/FinishedTeamPopupViewController.swift
+++ iteam_ny/Home/popVC/FinishedTeamPopupViewController.swift
@@ -13,6 +13,7 @@ import FirebaseStorage
 class FinishedTeamPopupViewController: UIViewController {
     
     @IBOutlet weak var contentView: UIView!
+    let db = Database.database().reference()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,8 +28,76 @@ class FinishedTeamPopupViewController: UIViewController {
     }
     
     @IBAction func onClickedYes(_ sender: Any) {
-        let userUID = Auth.auth().currentUser!.uid
-        Database.database().reference().child("user").child(userUID).child("userTeam").setValue("")
+        
+        let emptyCurrentTeam: [String:String] = ["currentTeam":""]
+        db.child("user").child(Auth.auth().currentUser!.uid)
+            .child("friendsList").removeValue()
+        db.child("user").child(Auth.auth().currentUser!.uid)
+            .child("friendRequest").removeValue()
+        db.child("user").child(Auth.auth().currentUser!.uid)
+            .child("likeTeam").removeValue()
+        db.child("user").child(Auth.auth().currentUser!.uid)
+            .child("memberRequest").removeValue()
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MM/dd HH:mm"
+        let currentDateString = formatter.string(from: Date())
+        
+        db.child("user").child(Auth.auth().currentUser!.uid)
+            .child("memberRequest").child("0").child("requestStmt").setValue("기본")
+        db.child("user").child(Auth.auth().currentUser!.uid)
+            .child("memberRequest").child("0").child("requestTime").setValue(currentDateString)
+        db.child("user").child(Auth.auth().currentUser!.uid)
+            .child("memberRequest").child("0").child("requestUID").setValue("기본")
+        
+        let emptyUserTeam: [String:String] = ["userTeam":""]
+        
+        db.child("user").child(Auth.auth().currentUser!.uid).updateChildValues(emptyUserTeam)
+   
+        
+        // call에서 본인 기록 있으면 찾아서 삭제
+        var callIndex: String = ""
+        db.child("Call").observeSingleEvent(of: .value) { [self] (snapshot) in
+            if let snapshots = snapshot.children.allObjects as? [DataSnapshot] {
+                callIndex = "\(snapshots.count)"
+                for i in 9...(Int(callIndex) ?? 9) {
+                    db.child("Call").child(String(i)).removeValue()
+                }
+                
+            }
+        }
+        
+        // 리더일 때 팀 삭제
+        db.child("user").child(Auth.auth().currentUser!.uid).observeSingleEvent(of: .value) { snapshot in
+            for child in snapshot.children {
+                let snap = child as! DataSnapshot
+                
+                if snap.key == "currentTeam" {
+                    let teamname: String = snap.value as? String ?? ""
+                    if teamname != nil && teamname != "" {
+                        self.db.child("Team").child(teamname).observeSingleEvent(of: .value) { snapshot in
+                            
+                            for child in snapshot.children {
+                                let snap = child as! DataSnapshot
+                                let value = snap.value as? String
+                                // 내가 리더일 때 팀 삭제
+                                if snap.key == "leader" {
+                                    if value == Auth.auth().currentUser!.uid {
+                                        print("리더")
+                                        self.db.child("Team").child(teamname).removeValue()
+                                        break
+                                    }
+                                }
+                            }
+                        }
+                    }
+        
+                }
+            }
+        }
+        db.child("user").child(Auth.auth().currentUser!.uid).updateChildValues(emptyCurrentTeam)
+        
+        
         
         self.dismiss(animated: true, completion: nil)
     }
