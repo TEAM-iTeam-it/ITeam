80850dfb8e1c151ea7a4127572605a13129a713e
angryeon7 <87401351+angryeon7@users.noreply.github.com>
2022-06-02 12:18:38 +0900

    [DESIGN]프로필화면 디자인수정

--- iteam_ny/Home/UserProfileController.swift
+++ iteam_ny/Home/UserProfileController.swift
@@ -2,7 +2,7 @@
 //  UserProfile.swift
 //  iteam_ny
 //
-//  Created by 성의연 on 2021/11/29.
+//  Created by 성나연 on 2021/11/29.
 //
 
 import UIKit
@@ -19,9 +19,12 @@ class UserProfileController: UIViewController{
     @IBOutlet weak var userImage: UIImageView!
     @IBOutlet weak var userName: UILabel!
     @IBOutlet weak var partLabel: UILabel!
-    @IBOutlet weak var characterLabel: UITextField!
-    @IBOutlet weak var characterLabel2: UITextField!
-    @IBOutlet weak var characterLabel3: UITextField!
+//    @IBOutlet weak var characterLabel: UITextField!
+//    @IBOutlet weak var characterLabel2: UITextField!
+//    @IBOutlet weak var characterLabel3: UITextField!
+    @IBOutlet weak var characterLabel1: UILabel!
+    @IBOutlet weak var characterLabel2: UILabel!
+    @IBOutlet weak var characterLabel3: UILabel!
     @IBOutlet weak var sameSchol: UITextField!
     
     @IBOutlet weak var Interest: UILabel!
@@ -55,22 +58,35 @@ class UserProfileController: UIViewController{
 
         navigationController?.navigationBar.scrollEdgeAppearance = fancyAppearance
         
+        
         backStack.layoutMargins = UIEdgeInsets(top: 15, left: 20, bottom: 30, right: 0)
         backStack.isLayoutMarginsRelativeArrangement = true
-        backStack.layer.cornerRadius = 10
+        backStack.layer.cornerRadius = 20
         
         backStack.layer.shadowColor = UIColor.black.cgColor // 색깔
-        backStack.layer.masksToBounds = false  // 내부에 속한 요소들이 UIView 밖을 벗어날 때, 잘라낼 것인지. 그림자는 밖에 그려지는 것이므로 false 로 설정
-        backStack.layer.shadowOffset = CGSize(width: 0, height: 4) // 위치조정
-        backStack.layer.shadowRadius = 5 // 반경
-        backStack.layer.shadowOpacity = 0.3 // alpha값
         
-//        let backgroundView = UIView()
-//
-//        backView.translatesAutoresizingMaskIntoConstraints = false
-//        backView.contentMode = .scaleAspectFit
-//        backView.addSubview(backgroundView)
-//        backgroundView.backgroundColor = UIColor.lightGray
+        backStack.layer.borderWidth = 0
+        backStack.layer.shadowColor = UIColor.black.cgColor
+        backStack.layer.shadowOffset = CGSize(width: 0, height: 0)
+        backStack.layer.shadowOpacity = 0.15
+        backStack.layer.shadowRadius = 10
+        
+        characterLabel1.layer.cornerRadius = characterLabel1.frame.height/2 + 5
+        characterLabel2.layer.cornerRadius = characterLabel2.frame.height/2 + 5
+        characterLabel3.layer.cornerRadius = characterLabel3.frame.height/2 + 5
+        characterLabel1.layer.borderWidth = 0.5
+        characterLabel1.layer.borderColor = UIColor.lightGray.cgColor
+        characterLabel2.layer.borderWidth = 0.5
+        characterLabel2.layer.borderColor = UIColor.lightGray.cgColor
+        characterLabel3.layer.borderWidth = 0.5
+        characterLabel3.layer.borderColor = UIColor.lightGray.cgColor
+        characterLabel1.clipsToBounds = true
+        characterLabel2.clipsToBounds = true
+        characterLabel3.clipsToBounds = true
+//        backStack.layer.masksToBounds = false  // 내부에 속한 요소들이 UIView 밖을 벗어날 때, 잘라낼 것인지. 그림자는 밖에 그려지는 것이므로 false 로 설정
+//        backStack.layer.shadowOffset = CGSize(width: 0, height: 4) // 위치조정
+//        backStack.layer.shadowRadius = 5 // 반경
+//        backStack.layer.shadowOpacity = 0.3 // alpha값
         
     }
     
@@ -84,19 +100,11 @@ class UserProfileController: UIViewController{
         let charindex = char.components(separatedBy: ", ")
         
         
-//            toolNlanguage.text = detail.language
-//            Interest.text = detail.interest
-//            date.text = detail.date
-//            projectDetail.text = detail.exDetail
-//            callTime.text = detail.calltime
-//            portfolioLabel.text = detail.portfolio
-//            contactLink.text = detail.contactLink
-        
         userName.text = basicinfo.nickname
         sameSchol.text = basicinfo.schoolName
         partLabel.text = basicinfo.partDetail
         purposeLabel.text = detail.purpose
-        characterLabel.text = charindex[0]
+        characterLabel1.text = charindex[0]
         characterLabel2.text = charindex[1]
         characterLabel3.text = charindex[2]
         if let portfolio = basicinfo.portfolio {
@@ -182,7 +190,7 @@ class ActualGradientButton2: UIButton {
         l.colors = [UIColor(displayP3Red: 184/255, green: 98/255, blue: 255/255, alpha: 1).cgColor, UIColor(displayP3Red: 144/255, green: 255/255, blue: 201/255, alpha: 1).cgColor]
         l.startPoint = CGPoint(x: 0, y: 0.5)
         l.endPoint = CGPoint(x: 1, y: 0.5)
-        l.cornerRadius = 16
+        l.cornerRadius = 8
         layer.insertSublayer(l, at: 0)
         return l
     }()
